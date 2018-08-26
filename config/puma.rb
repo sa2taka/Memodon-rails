138fad9a9f336e43266380ebc1b5e6336dba9fc8
sa2taka <sa2taka@gmail.com>
2018-08-27 00:33:42 +0900

    追加: rails実行時にsidekiqが実行されるように

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
@@ -15,20 +16,15 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+preload_app!
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+on_worker_boot do
+  @sidekiq_pid ||= spawn('bundle exec sidekiq -C config/sidekiq.yml')
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
 
-# Allow puma to be restarted by `rails restart` command.
+on_restart do
+  Sidekiq.redis.shutdown { |conn| conn.close }
+end
+
 plugin :tmp_restart
