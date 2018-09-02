d64bb19fd7e43a08a15433ad327d616c8becd3b4
sa2taka <sa2taka@gmail.com>
2018-09-02 15:42:35 +0900

    修正: timelineを取得するのにaccess_tokenは必要ないので、それを修正

--- app/memodon_tools/memo_crawler.rb
+++ app/memodon_tools/memo_crawler.rb
@@ -2,9 +2,9 @@
 # crawlメソッドで自動的に(未登録の)メモ(と未登録のアカウント)をinsertする
 class MemoCrawler
   class << self
-    def crawl(instance_url, username, access_token, max_id = nil)
+    def crawl(instance_url, username, max_id = nil)
       latest_id = User.find(username: username).memos.order('status_id desc').first&.status_id || 1
-      client = MstdnIvory::Client.new("https://#{instance_url}", access_token)
+      client = MstdnIvory::Client.new("https://#{instance_url}")
       timeline = client.get('/api/v1/timelines/tag/メモ', { local: 'true', since_id: latest_id, max_id: max_id, limit: 40 })
       insert_database(timeline)
 
