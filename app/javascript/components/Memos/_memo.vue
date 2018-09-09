d654a787856a926aeb12524a8afb43b0ae1b47c5
sa2taka <sa2taka@gmail.com>
2018-09-09 23:56:40 +0900

    修正: memo.idに関する修正

---
+++ app/javascript/components/Memos/_memo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="memo" class="col s10 offset-s1">
+    <div class="card orange lighten-3">
+      <div class="card-content black-text">
+        <div class="status-content" v-html="memo.text">
+
+        </div>
+        <div class="row actions">
+          <div class="col s6 center go-to-toot">
+            <a :href="https://mstdn-workers.com/web/statuses/${memo.id}" class="card-link">
+              <i class="material-icons memo-link-icon">subdirectory_arrow_right</i>
+            </a>
+          </div>
+          <div class="col s6 center delete-memo">
+            <a class="card-link" :click="deleteMemo(memo.id)">
+              <i class="material-icons memo-link-icon">delete_forever</i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'memo',
+  data () {
+    return {
+    }
+  },
+  props: {
+    memo: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
