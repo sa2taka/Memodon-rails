992b0707919543575413577a92f5ec54609ff6f3
sa2taka <sa2taka@gmail.com>
2018-09-15 15:07:07 +0900

    追加: APIのmemosにtag_nameを指定して検索出来るように

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -1,15 +1,17 @@
 class Api::MemosController < ApplicationController
   def index
     render json: login_error if session[:user_id].nil?
-    render json: user_memos, each_serializer: MemosSerializer
+    render json: create_memos, each_serializer: MemosSerializer
   end
 
   private
 
-  def user_memos
-    page    = params[:page] || 1
-    size    = params[:size] || Constants::DEFAULT_PAGE_SIZE
-    user_id = session[:user_id]
+  def create_memos
+    page     = params[:page] || 1
+    size     = params[:size] || Constants::DEFAULT_PAGE_SIZE
+    user_id  = session[:user_id]
+    tag_name = params[:tag_name]
+    return find_user_memos_with_tag(user_id, page, size, tag_name) unless tag_name.nil?
     find_user_memos(user_id, page, size)
   end
 
@@ -23,6 +25,17 @@ class Api::MemosController < ApplicationController
       .memos
       .page(page)
       .per(size)
+      .order('memos.status_id desc')
+  end
+
+  def find_user_memos_with_tag(user_id, page, size, tag_name)
+    User
+      .find(user_id)
+      .memos
+      .page(page)
+      .per(size)
+      .joins(:tags)
+      .where(['tags.name = ?', tag_name])
       .order('memos.status_id desc')
   end
 end
