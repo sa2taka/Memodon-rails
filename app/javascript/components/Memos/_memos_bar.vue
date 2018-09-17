94f15f5a78df5decd42c7ca4fa778edc9ad36c2c
sa2taka <sa2taka@gmail.com>
2018-09-17 17:46:37 +0900

    追加: menu barからmenu bottunを分離してカラムのトグルをそこに追加

--- app/javascript/components/Memos/_memos_bar.vue
+++ app/javascript/components/Memos/_memos_bar.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="memos-bar row">
     <a class="back_to_tags" @click="back_to_tags" v-if="isTagMemos">&lt;&lt;タグ一覧へ</a>
-    <a class="btn col offset-s1 s2" @click="_test_crawl">クロール</a>
-    <div class="switch col offset-s3 s5">
-      <label class="column-switch">
-        単独カラム
-        <input type="checkbox" checked @click="$emit('toggleColumn')">
-        <span class="lever"></span>
-        複数カラム
-      </label>
+    <a class="btn btn-waves" @click="_test_crawl">クロール</a>
+    <div class="switch bar-menu">
+      <a id="menu" class="waves-effect waves-light btn btn-floating cyan darken-1 bar-menu__btn" @click="openMenu">
+        <i class="material-icons">menu</i>
+      </a>
+      <div class="tap-target cyan darken-1 rotation" data-target="menu">
+        <div class="tap-target-content">
+          <label class="column-switch  white-text">
+            単独カラム
+            <input type="checkbox" checked @click="$emit('toggleColumn')">
+            <span class="lever"></span>
+            複数カラム
+          </label>
+          <br />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,9 +26,13 @@ export default {
   name: 'memos_bar',
   data () {
     return {
+      instance: Object
     }
   },
   mounted () {
+    const elems = document.querySelectorAll('.tap-target')
+    const options = {}
+    this.instance = M.TapTarget.init(elems, options)[0]
   },
   methods: {
     _test_crawl: function () {
@@ -31,6 +43,9 @@ export default {
     },
     back_to_tags: function () {
       location.href = "/#/tags"
+    },
+    openMenu: function () {
+      this.instance.open()
     }
   },
   computed: {
@@ -44,9 +59,13 @@ export default {
 </script>
 
 <style scoped>
+.tap-target {
+  overflow: hidden;
+}
+
 .column-switch {
   font-size: 22px;
-  font-weight: 600;
+  font-weight: 400;
   color: rgb(51, 51, 51);
 }
 
@@ -54,7 +73,28 @@ export default {
   cursor: pointer;
 }
 
-.back_to_tags:hover {
-  text-decoration: underline;
+.bar-menu {
+  width: 90%;
+  position: fixed;
+  z-index: 9999;
+  bottom: 96px;
+}
+
+.bar-menu__btn {
+  position: absolute;
+  right: 48px;
+}
+
+.tap-target-content > a {
+  color: #039be5!important;
+  font-size: 22px;
+}
+
+.rotation {
+  transform: rotate(90deg) scale(0.1);
+}
+
+.open .rotation {
+  transform: rotate(12deg) scale(1.0)!important;
 }
 </style>
