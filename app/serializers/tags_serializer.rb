812814a39758b66f8841f2552b763ed4a3917bde
sa2taka <sa2taka@gmail.com>
2018-09-24 21:09:26 +0900

    変更: tags_serializerをname, count, text全て入れる様に変更

--- app/serializers/tags_serializer.rb
+++ app/serializers/tags_serializer.rb
@@ -1,3 +1,32 @@
 class TagsSerializer < ActiveModel::Serializer
-  attributes :name
+  attributes :name, :count, :text
+
+  def count
+    Tag
+      .joins(:memos)
+      .select('count(*) as count')
+      .where(['memos.user_id = ? and tags.name = ?', user_id, name])
+      .group('tags.name')
+      .first
+      .count
+  end
+
+  def text
+    Tag
+      .joins(:memos)
+      .select('memos.text')
+      .where(['memos.user_id = ? and tags.name = ?', user_id, name])
+      .group('memos.text, memos.status_id')
+      .order('memos.status_id desc')
+      .first
+      .text
+  end
+
+  def user_id
+    scope[:user_id]
+  end
+
+  def name
+    object.name
+  end
 end
