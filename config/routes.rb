5b9e3f6a7e43c33128dfaa2714450efb42541c93
sa2taka <sa2taka@gmail.com>
2018-09-24 21:13:08 +0900

    変更: routesをtags_controllerの変更に合わせて変更

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,6 @@ Rails.application.routes.draw do
     get 'users', to: 'users#index'
     get 'current_user', to: 'users#current_user'
     get 'tags', to: 'tags#index'
-    get 'tags/recent_tags', to: 'tags#recent'
-    get 'tags/most_tags', to: 'tags#most'
   end
 
   get '*path', to: redirect('/')
